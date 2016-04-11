7779740d48df9b9218ac4c040efa22cfaf974a2d
尹庆文 <yinqingwen@gmail.com>
2016-04-11 12:21:49 +0800

    更改设置系统

---
+++ Homgmen/Areas/Setting/Controllers/SettingController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Homgmen.Models;
+
+namespace Homgmen.Areas.Setting.Controllers
+{
+    public class SettingController : Controller
+    {
+        /// <summary>
+        /// iPad单据数据库，IP地址为182.92.174.109，库名：adtic
+        /// </summary>
+        private OldSot oldsot = new OldSot();
+
+        // GET: Setting/Setting
+        public ActionResult Index()
+        {
+            ViewBag.iPadCount = oldsot.Iddes.Where(item => item.完成度 == "2").Count();
+            ViewBag.SiteCount = oldsot.Citytels.Where(item => item.完成度 == "2").Count();
+            return View();
+        }
+    }
+}
