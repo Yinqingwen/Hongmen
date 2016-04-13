0d921273a4e079bb060441efd767edd05cd0ae22
尹庆文 <yinqingwen@gmail.com>
2016-04-13 16:57:23 +0800

    完善大红门提交

--- Homgmen/Scripts/index.js
+++ Homgmen/Scripts/index.js
@@ -1,17 +1,22 @@
-﻿/*****************************************
-* 创建数据对象类                         *
-*****************************************/
-var resultTable = new Object();
-
+﻿/******************************************
+* 页面载入后的初始化函数                  *
+******************************************/
 $().ready(function () {
     //为提交数据按钮设置响应函数
+    $('#DHMGroupSideMenu_Index').addClass('active');
     $("#Upload").click(datawork);
 });
 
+/******************************************
+* 显示状态文本                            *
+******************************************/
 function displayzt(xs) {
     $("#zhuangtai").text(xs);
 }
 
+/******************************************
+* 更改状态进度条                          *
+******************************************/
 function displayjd(fen) {
     var bfb = fen + "%";
     $(".progress-bar").css("width", bfb);
@@ -22,16 +27,17 @@ function displayjd(fen) {
 * 数据准备函数                            *
 ******************************************/
 function datawork() {
+    $("#Upload").attr("disabled",true);
     displayzt("开始准备数据");
-    displayjd(8);
+    displayjd(1);
     //获取单据数目
     $.ajax({
         url: 'DHMGroup/Site/ReadyData',
         success: function (data) {
             var xx = "今日应提交" + data + "条数据！！！";
-            resultTable.TodayRowNumber = data;
             displayzt(xx);
-            displayjd(16);
+            displayjd(20);
+            //从iPad数据库同步到上传数据库
             syncdata();
         }
     });
@@ -42,14 +48,14 @@ function datawork() {
 ******************************************/
 function syncdata() {
     displayzt("开始同步数据......");
-    displayjd(24);
+    displayjd(21);
     //同步数据库
     $.ajax({
-        url: '/Home/SyncData',
+        url: 'DHMGroup/Site/SyncData',
         success: function (data) {
             var xx = "已经同步" + data + "条数据！！！";
             displayzt(xx);
-            displayjd(32);
+            displayjd(40);
             //上传今日数据函数
             uploaddata()
         }
@@ -61,30 +67,13 @@ function syncdata() {
 ******************************************/
 function uploaddata() {
     displayzt("开始向大红门集团上传数据......");
-    displayjd(40);
+    displayjd(41);
     $.ajax({
-        url: '/Home/UploadData',
+        url: 'DHMGroup/Site/UploadData',
         success: function (data) {
             var xx = "已经向大红门集团上报" + data + "条数据！！！";
             displayzt(xx);
-            displayjd(48);
-            getdsjine();
-        }
-    });
-}
-
-/******************************************
-* 获取当日代收金额函数                    *
-******************************************/
-function getdsjine() {
-    displayzt("开始获取当日代收金额......");
-    displayjd(56);
-    $.ajax({
-        url: '/Home/GetDSJine',
-        success: function (data) {
-            var xx = "今日代收金额为人民币" + data + "元整!!!";
-            displayzt(xx);
-            displayjd(64);
+            displayjd(60);
             UploadDhData();
         }
     });
@@ -95,13 +84,13 @@ function getdsjine() {
 ******************************************/
 function UploadDhData() {
     displayzt("开始向大红门集团上报当日到货单据......");
-    displayjd(76);
+    displayjd(61);
     $.ajax({
-        url: '/Home/UploadDH',
+        url: 'DHMGroup/Site/UploadDH',
         success: function (data) {
             var xx = "今日到货单据为" + data + "条，已上报大红门集团！！！";
             displayzt(xx);
-            displayjd(84);
+            displayjd(80);
             UploadFK();
         }
     });
@@ -112,13 +101,14 @@ function UploadDhData() {
 ******************************************/
 function UploadFK() {
     displayzt("开始向大红门集团上报单日汇款数据......");
-    displayjd(92);
+    displayjd(81);
     $.ajax({
-        url: '/Home/UploadFK',
+        url: 'DHMGroup/Site/UploadFK',
         success: function (data) {
             var xx = "今日付款单据数量为" + data + "条，已上报大红门集团";
             displayzt(xx);
             displayjd(100);
+            window.location.href = "DHMGroup";
         }
     });
 }
