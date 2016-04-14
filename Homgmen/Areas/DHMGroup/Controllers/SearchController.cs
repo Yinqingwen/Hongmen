6c73f537895b9df839f0a9d74299062208682762
尹庆文 <yinqingwen@gmail.com>
2016-04-14 18:01:59 +0800

    2016041401

---
+++ Homgmen/Areas/DHMGroup/Controllers/SearchController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Homgmen.Models;
+
+namespace Homgmen.Areas.DHMGroup.Controllers
+{
+    public class SearchController : Controller
+    {
+        /// <summary>
+        /// 上传运单数据库
+        /// </summary>
+        private NewSot newdb = new NewSot();
+
+        // GET: DHMGroup/Search
+        public ActionResult Yundan()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Yundan(long YdNumber)
+        {
+            var data = newdb.sothms.Find(YdNumber);
+
+            return View(data);
+        }
+    }
+}
